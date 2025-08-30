2ae46573accb0f9b37834860c9d4948cfbdb04db
Erick Lobo <erickloboabreu@hotmail.com>
2025-08-30 12:19:13 -0300

    add validations

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,6 @@
 class Shift < ApplicationRecord
   belongs_to :volunteer
   belongs_to :event
+  validates :feedback, length: { minimum: 6 }
+  validates :volunteer, uniqueness: { scope: :event }
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,4 @@
 class Volunteer < ApplicationRecord
+  has_many :shifts
+  validates :name, presence: true, uniqueness: true
 end
